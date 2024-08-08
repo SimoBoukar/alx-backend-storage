7b7aaea123c6cd67fa3992e35b2e4195799fc726
Mohammed <6bouass9@gmail.com>
2024-08-08 23:04:45 +0100

    push task 2 + 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
--- Ranks country origins of bands, ordered by the number of (non-unique) fans.
-SELECT origin, SUM(fans) AS nb_fans
-	FROM metal_bands
-	GROUP BY origin
-	ORDER BY nb_fans DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
