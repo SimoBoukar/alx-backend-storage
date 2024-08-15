4e6bc5178ad2b9665eca06e37b19da74ff208fd4
Mohammed <6bouass9@gmail.com>
2024-08-15 01:18:34 +0100

    push advanced task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -69,8 +69,7 @@ class Cache:
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''Stores a value in a Redis data storage and returns the key.
-            '''
+        '''Stores a value in a Redis data storage and returns the key.'''
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
         return data_key
