055678c22673eb2135096da7d4ea2a9aa0af008a
Mohammed <6bouass9@gmail.com>
2024-08-15 01:52:24 +0100

    push mandatory + advanced task v2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -46,36 +46,34 @@ class Cache:
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ generate a random key (e.g. using uuid), store the input data in
-        Redis using the random key and return the key """
+        """ to count how many times methods of the Cache class are called """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-def get(self, key: str,
-        fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """ take a key string argument and an optional Callable argument named
         fn. This callable will be used to convert the data back to the
         desired format """
         data = self._redis.get(key)
         if fn:
-             return fn(data)
+            return fn(data)
         return data
 
-def get_str(self, key: str) -> str:
+    def get_str(self, key: str) -> str:
         """ automatically parametrize Cache.get to str """
         data = self._redis.get(key)
         return data.decode("utf-8")
 
-def get_int(self, key: str) -> int:
+    def get_int(self, key: str) -> int:
         """ automatically parametrize Cache.get to int """
         data = self._redis.get(key)
         try:
-            data = int(value.decode("utf-8"))
+            data = int(data.decode("utf-8"))
         except Exception:
             data = 0
         return data
-
 
 def replay(method: Callable):
     """ display the history of calls of a particular function """
